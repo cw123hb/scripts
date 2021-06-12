dd628f33cd83b7d41c890c96d8115398e198c67c
cw123hb <73165681+cw123hb@users.noreply.github.com>
2021-06-12 07:31:50 +0200

    Update nodejs_install.sh

--- nodejs_install.sh
+++ nodejs_install.sh
@@ -1,2 +1,9 @@
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs
+echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
+. ~/.bashrc
+mkdir ~/local
+mkdir ~/node-latest-install
+cd ~/node-latest-install
+curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
+./configure --prefix=~/local
+make install # ok, fine, this step probably takes more than 30 seconds...
+curl https://www.npmjs.org/install.sh | sh
