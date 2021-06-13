1dec642828595d15ad1edda6227f6a15d86394f1
Michelle <mc@uix.dk>
2021-06-13 06:39:32 +0200

    ok
    Ok

--- essentials.sh
+++ essentials.sh
@@ -8,38 +8,15 @@ sudo apt-get install -y \
   software-properties-common \
   wget
 
-sudo add-apt-repository -y ppa:jonathonf/vim
-sudo add-apt-repository -y ppa:fish-shell/release-3
-sudo add-apt-repository -y ppa:git-core/ppa
 sudo apt-get update
 
 sudo apt-get install -y \
-  aptitude \
-  aria2 \
-  aspell \
-  cmake \
-  fd-find \
-  fish \
   git \
   htop \
-  httpie \
-  jq \
-  mariadb-client \
-  mcrypt \
-  memcached \
   mycli \
-  ncdu \
-  ntp \
-  ntpdate \
-  p7zip \
   pgcli \
   python3-pip \
-  python3-tmuxp \
-  python3-venv \
   rsync \
-  ruby \
-  ruby-dev \
-  silversearcher-ag \
   software-properties-common \
   tig \
   tmux \
@@ -47,12 +24,4 @@ sudo apt-get install -y \
   unrar \
   unzip \
   vim \
-  virtualenv \
-  wajig \
-  wbritish \
-  xz-utils
 
-sudo sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen &&
-  dpkg-reconfigure --frontend=noninteractive locales &&
-  update-locale LANG=en_US.UTF-8
-sudo ln -fs /usr/share/zoneinfo/Australia/Sydney /etc/localtime
